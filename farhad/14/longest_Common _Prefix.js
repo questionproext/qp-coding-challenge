2c22db8a7aac23ce01361fc16de727a1036b2f82
Md Farhad Hossain <farhad@Mds-MacBook-Pro.local>
2025-08-20 15:48:16 +0600

    Leetcode problem 66 is done

---
+++ farhad/14/longest_Common _Prefix.js
@@ -0,0 +1,12 @@
+// const longestCommonPrefix = (strs) => {
+//   let logestPrefix = "";
+//   for (let i = 0; i < strs.length; i++) {
+//     for (let j = 0; j < strs[i].length; j++) {
+//       //   if(strs[i][j] === strs[i]);
+//       console.log(strs[i][j]);
+//     }
+//   }
+// };
+
+// const strs = ["flower", "flow", "flight"];
+// longestCommonPrefix(strs);
